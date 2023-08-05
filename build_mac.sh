0785604f8775e24584d46d785b1409ba57fea73e
Will Hartung <willhartung@gmail.com>
2023-08-05 14:56:56 -0700

    Fixed the Launcher package.

--- build_mac.sh
+++ build_mac.sh
@@ -8,4 +8,4 @@ cp target/Planet-1.0-SNAPSHOT.jar target/installer/input/libs/
 jdeps --multi-release 17 --ignore-missing-deps --print-module-deps --class-path "target/installer/input/libs/*" target/classes
 modules="java.base,java.net.http,java.scripting,jdk.jfr,jdk.jsobject,jdk.unsupported,jdk.unsupported.desktop,jdk.xml.dom,java.logging"
 jlink --strip-native-commands --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules "$modules" --output target/java-runtime
-jpackage --type dmg --dest target/installer --input target/installer/input/libs/ --icon icons/planet.icns --name Planet --main-class bit.planet.Launcher --main-jar Planet-1.0-SNAPSHOT.jar --runtime-image target/java-runtime
+jpackage --type dmg --dest target/installer --input target/installer/input/libs/ --icon icons/planet.icns --name Planet --main-class app.wnh.planet.Launcher --main-jar Planet-1.0-SNAPSHOT.jar --runtime-image target/java-runtime
